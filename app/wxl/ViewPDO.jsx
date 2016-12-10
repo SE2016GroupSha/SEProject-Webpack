7bb7d9211c7e13da4dd144ef8b0c8d9251516349
fanfan111 <596121814@qq.com>
2016-12-10 23:26:44 +0800

    ..

--- app/wxl/ViewPDO.jsx
+++ app/wxl/ViewPDO.jsx
@@ -83,34 +83,7 @@ var ViewPDO = React.createClass( {
     },
 	downloadHandle: function(){
 		//模板下载接口
-		var downloadIndex = this.props.index;
-		const fs = require('fs');
-		const xlsx = require('better-xlsx');
-
-		const file = new xlsx.File();
-
-		const sheet = file.addSheet('Sheet1');
-		const row = sheet.addRow();
-		const cell = row.addCell();
-
-		cell.value = 'I am a cell!';
-		cell.hMerge = 2;
-		cell.vMerge = 1;
-
-		const style = new xlsx.Style();
-
-		style.fill.patternType = 'solid';
-		style.fill.fgColor = '00FF0000';
-		style.fill.bgColor = 'FF000000';
-		style.align.h = 'center';
-		style.align.v = 'center';
-
-		cell.style = style;
-
-		file
-		  .saveAs()
-		  .pipe(fs.createWriteStream('test.xlsx'))
-		  .on('finish', () => console.log('Done.'));
+		
 	},
     render: function() {
 		var theleftwindowclassname='col w-lg lt b-r ';
